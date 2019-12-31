e3a2214ad2873207b5780a947a28edd879f75e73
OVRIDOO <ugur.altnsy@gmail.com>
2019-12-31 19:31:30 +0300

    Android Studio İlk Ders

---
+++ app/src/main/java/com/deeplab/androidstudioders1/Main.java
@@ -0,0 +1,44 @@
+package com.deeplab.androidstudioders1;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        //INTEGER
+        int x = 1;
+        int y = 61;
+
+        System.out.println(x);
+        System.out.println(y);
+
+        //double
+
+        double db = 11.5;
+        double a = 3.14;
+        System.out.println(db);
+        System.out.println(a);
+
+        //float
+
+        float flot = 18.5f;
+        System.out.println(flot);
+
+        //String
+
+        String name = "Ugur";
+        String soyisim = "Altınsoy";
+
+        System.out.println(name);
+        System.out.println(soyisim);
+
+        System.out.println(name + soyisim);
+        System.out.println(name + " " + soyisim);
+
+        //Boolean
+        boolean blod = false;
+        System.out.println(blod);
+        blod = true;
+        System.out.println(blod);
+    }
+
+}
